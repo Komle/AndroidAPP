3f9bb50ba406fd1277ae82c99c75a2e48354af96
Moksleivis <Moksleivis@KITM.local>
2020-09-16 12:27:51 +0300

    vaziuoja

--- app/src/main/java/com/example/bandom/LoginActivity.java
+++ app/src/main/java/com/example/bandom/LoginActivity.java
@@ -9,8 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
-public class Login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +19,20 @@ public class Login extends AppCompatActivity {
         final EditText username = findViewById(R.id.username);
         final EditText password = findViewById(R.id.password);
         Button loginBtn = findViewById(R.id.Login_btn);
+        Button registerBtn = findViewById(R.id.Register);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username2 = username.getText().toString();
                 String password2 = password.getText().toString();
-                Toast.makeText(Login.this, "Username: " + username2 + "\n" + "Password: " + password2, Toast.LENGTH_SHORT).show();
-
-                Intent gotoSearch = new Intent(Login.this, Search.class);
-                startActivity(gotoSearch);
+                Toast.makeText(LoginActivity.this, "Username: " + username2 + "\n" + "Password: " + password2, Toast.LENGTH_SHORT).show();
 
                 username.setError(null);
                 password.setError(null);
                 if (Validation.isCredentialsValid(username2) && Validation.isCredentialsValid(password2)) {
 
-                    Intent gotoSearch = new Intent(Login.this, Search.class);
+                    Intent gotoSearch = new Intent(LoginActivity.this, Search.class);
                     startActivity(gotoSearch);
                 } else {
                     username.setError(getResources().getString(R.string.login_invalid_credentials_message));
@@ -43,5 +40,14 @@ public class Login extends AppCompatActivity {
                 }
             }
         });
+
+        registerBtn.setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View view) {
+        Intent gotoRegisterActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(gotoRegisterActivity);
+    }
+});
+
     }
 }
--- app/src/main/java/com/example/bandom/Validation.java
+++ app/src/main/java/com/example/bandom/Validation.java
@@ -9,9 +9,25 @@ import java.util.regex.Pattern;
 
 public class Validation {
     private static final String CREDENTIALS_PATTERN = "^[a-zA-Z]{3,20}$";
+
     public static boolean isCredentialsValid(String credentials){
         Pattern pattern= Pattern.compile(CREDENTIALS_PATTERN);
         Matcher matcher= pattern.matcher(credentials);
+        return matcher.matches();
+    }
+
+    private static final String CREDENTIALS_PATTERN2 = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+
+    public static boolean isEmailValid(String emails){
+        Pattern pattern= Pattern.compile(CREDENTIALS_PATTERN2);
+        Matcher matcher = pattern.matcher(emails);
+        return matcher.matches();
+    }
+    private static final String CREDENTIALS_PATTERN3 = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{5,20}$";
+
+    public static boolean isPasswordValid(String passwords){
+        Pattern pattern= Pattern.compile(CREDENTIALS_PATTERN3);
+        Matcher matcher = pattern.matcher(passwords);
         return matcher.matches();
     }
 }
